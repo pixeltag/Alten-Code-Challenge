d11945428ed00adb2d3cb7b3218840eefdca79e9
ihab Youssef <pixeltagnet@gmail.com>
2019-11-11 20:22:49 +0300

    Setup the server, Socket IO , sending dummy data

---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const data = require('./db/data.json')
+// port from envirnoment var or default - 3001
+const port = process.env.PORT || 3001;
+
+// setting up express
+const app = express();
+
+// create the server
+const server = http.createServer(app);
+
+// adding socket IO middleware
+const io = socketIo(server);
+
+// constant for timer interval
+const time = 1000;
+
+// handle home request
+app.get('/' , function(req , res) {
+    res.sendFile(__dirname + 'index.html');
+})
+
+// setting up a socket with the namespace "connection" for new sockets
+io.on('connection' , socket => {
+    console.log('Socket is connected ..');
+
+    // listen on a new namespace called "incoming"
+    socket.on('incoming' , data => {
+        // here we broadcast it out
+        console.log(data , 'incoming data');
+    });
+
+    // emiting the data every one minute
+    setInterval(() => {
+        socket.emit('sendingData' , data);
+    } , time)
+
+    // namespace 'disconnect' when a client disconnects
+    socket.on('disconnect', () => {
+        console.log('Socket is disconnected ..');
+    });
+});
+
+// run the server with the port
+server.listen(port , () => console.log(`Listening on port ${port}`));
