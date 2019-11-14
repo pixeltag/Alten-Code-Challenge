8bf950c0297439e32063bbea3b26dd788dc00818
Ihab Muhammed Youssef <902519@mof.gov.sa>
2019-11-14 13:33:59 +0300

    Fixed Redux-devtools-extension error

--- smart-tracer/src/store/index.js
+++ smart-tracer/src/store/index.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 const initialState = {};
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-);
+const middlewareEnhancer = applyMiddleware(thunk);
+const composedEnhancers = compose(middlewareEnhancer, composeWithDevTools);
+
+const store = createStore(rootReducer, initialState, composedEnhancers);
 
 export default store;
